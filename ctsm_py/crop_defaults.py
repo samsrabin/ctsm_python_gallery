9ebbb47fcf00d6493f7a3282fb2f08776d029a63
Sam Rabin <sam.rabin@gmail.com>
2025-03-07 13:54:07 -0700

    Change default var names to match CTSM outputs.

--- ctsm_py/crop_defaults.py
+++ ctsm_py/crop_defaults.py
@@ -7,9 +7,9 @@ from types import MappingProxyType
 # MappingProxyType makes this dict immutable
 DEFAULT_VAR_DICT = MappingProxyType(
     {
-        "hui_var": "HUI",
-        "huifrac_var": "HUIFRAC",
-        "gddharv_var": "GDDHARV",
-        "gslen_var": "GSLEN",
+        "hui_var": "HUI_PERHARV",
+        "huifrac_var": "HUIFRAC_PERHARV",
+        "gddharv_var": "GDDHARV_PERHARV",
+        "gslen_var": "GSLEN_PERHARV",
     }
 )
